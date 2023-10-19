cc39bdac1285f1162a9e0fb02d2f07663ed29035
lszydlo <szydler@gmail.com>
2023-10-19 21:05:01 +0200

    Fix gradle deprecation warning

--- parser/build.gradle.kts
+++ parser/build.gradle.kts
@@ -21,9 +21,11 @@ repositories {
 
 dependencies {
     implementation("io.github.classgraph:classgraph:4.8.162")
+    implementation("com.google.code.gson:gson:2.10.1")
     implementation("org.p3model:p3-model-annotations:0.1-SNAPSHOT")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("io.hosuaby:inject-resources-junit-jupiter:0.3.2")
     testImplementation("ch.qos.logback:logback-classic:1.4.11")
