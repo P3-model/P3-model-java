18bfc03216ffa8b62bdbe190ccafa6c48ead624d
lszydlo <szydler@gmail.com>
2023-10-20 08:54:44 +0200

    Add meta-annotation ModelElement and SystemDefinition annotation

--- annotations/src/main/java/org/p3model/annotations/domain/dynamicModel/ddd/DddApplicationService.java
+++ annotations/src/main/java/org/p3model/annotations/domain/dynamicModel/ddd/DddApplicationService.java
@@ -1,5 +1,11 @@
 package org.p3model.annotations.domain.dynamicModel.ddd;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import org.p3model.annotations.ModelElement;
+
+@ModelElement
+@Target(ElementType.TYPE)
 public @interface DddApplicationService {
     String name() default "";
 
--- annotations/src/main/java/org/p3model/annotations/domain/staticModel/ddd/DddAggregate.java
+++ annotations/src/main/java/org/p3model/annotations/domain/staticModel/ddd/DddAggregate.java
@@ -2,11 +2,13 @@ package org.p3model.annotations.domain.staticModel.ddd;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Target;
+import org.p3model.annotations.ModelElement;
 
 /**
  * Indicates that given type is a DDD Aggregate
  * If you want to learn more check <a href="https://www.domainlanguage.com/ddd/reference/">DDD Reference by Eric Evans</a>
  */
+@ModelElement
 @Target(ElementType.TYPE)
 public @interface DddAggregate {
     String name() default "";
