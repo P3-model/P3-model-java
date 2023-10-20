81bc08a9132db21ed6de8c4581dc9f5e77c74202
lszydlo <szydler@gmail.com>
2023-10-20 11:08:06 +0200

    Fix multimodule sample project

--- parser/src/test/java/org/p3model/samples/multimodule/module1/SampleM1.java
+++ parser/src/test/java/org/p3model/samples/multimodule/module1/SampleM1.java
@@ -1,5 +1,8 @@
 package org.p3model.samples.multimodule.module1;
 
+import org.p3model.annotations.domain.staticModel.ddd.DddEntity;
+
+@DddEntity
 public class SampleM1 {
 
 }
--- parser/src/test/java/org/p3model/samples/multimodule/module2/SampleM2.java
+++ parser/src/test/java/org/p3model/samples/multimodule/module2/SampleM2.java
@@ -1,5 +1,8 @@
 package org.p3model.samples.multimodule.module2;
 
+import org.p3model.annotations.domain.staticModel.ddd.DddEntity;
+
+@DddEntity
 public class SampleM2 {
 
 }
